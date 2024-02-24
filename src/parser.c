fa516a41f833015ad0a627b214bf75f585285cda
Alise Arponen <aarponen@c4b11c1.42berlin.de>
2024-02-24 17:40:27 +0100

    working on the quote thing

--- src/parser.c
+++ src/parser.c
@@ -3,12 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aarpo e  <aarponen@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: aarponen <aarponen@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 19:46:01 by aarpo e           #+#    #+#             */
-/*   Updated: 2024/02/21 20:00:12 by aarpo e          ###   ########.fr       */
+/*   Updated: 2024/02/24 16:56:05 by aarponen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "minishell.h"
+
+// put quotes together
+// ft_organize_quotes(t_lexer *lexer)
+// {
+//     t_lexer *tmp;
+//     t_lexer *new;
+//     t_lexer *head;
+
+//     tmp = lexer;
+//     while (tmp)
+//     {
+//         if (tmp->token == "DQUOTE" || tmp->token == "SQUOTE")
+//         {
+//             new = ft_init_lexer(new, tmp->index);
+//             new->str = tmp->str;
+//             new->token = tmp->token;
+//             if (tmp->index == 0)
+//                 head = new;
+//             else
+//             {
+//                 head->next = new;
+//                 new->prev = head;
+//             }
+//             head = new;
+//             tmp = tmp->next;
+//         }
+//         else
+//             tmp = tmp->next;
+//     }
+//     return (head);
+// }
+
 
 // group nodes together based on tokens
 // each group becomes command
@@ -21,3 +55,4 @@
 // remove the redirection nodes from lexer
 // check if first command is a builtin and store a function pointer to the corresponding function
 // combine remaining words into **char for execve
+
