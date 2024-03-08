85baf82d0a6bbf1a5d37dc0eb50b5e64f432f72c
Luis Fernando Perez Herrera <lperez-h@c4c1c2.42berlin.de>
2024-03-08 22:36:52 +0100

    small progress

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -6,13 +6,13 @@
 /*   By: lperez-h <lperez-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 11:51:04 by lperez-h          #+#    #+#             */
-/*   Updated: 2024/03/08 22:18:10 by lperez-h         ###   ########.fr       */
+/*   Updated: 2024/03/08 22:36:24 by lperez-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_cd(t_cmd *cmds)
+int	ft_update_pwd(t_cmd *cmds)
 {
 	char	buffer[PATH_MAX];
 	char	*tmp_1;
@@ -24,18 +24,18 @@ int	ft_cd(t_cmd *cmds)
 		tmp_1 = ft_strdup(buffer, cmds->data);//copy the current work directory into temporary
 		tmp_2 = ft_strjoin(cmds->cmd_arg, "=", cmds->data);//copy the input str (arguments received + = sign)
 		cmds->data->env = ft_strjoin(tmp_1, tmp_2, cmds->data);//check how do I update the value of the env variable inside the **env array
-		free(tmp_1);
-		free(tmp_2);
+		free(tmp_1);//free the allocated memory in strdup and strjoin
+		free(tmp_2);//free the allocated memory in strdup and strjoin
 	}
-
-	path = cmds->cmd_arg;//Here I get the path where I want to move
-
-	if(!path)
-	{
-		if(HOME)//
-			chdir(HOME);
-	}
+	return(1);
 }
+
+int	ft_change_dir(t_cmd *cmds)
+{
+	ft_update_pwd(cmds->cmd_arg);//what parameter it takes here? cmds->cmd_arg? previous
+	if(!chdir(cmds->cmd_arg) && ft_update_pwd());//current. Maybe use G variables? PWD + OLDPWD?
+}
+
 
 /*
 1) update enviroment variables 
