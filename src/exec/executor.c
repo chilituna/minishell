2a41186bf651768ef1d34894fe88101d22e7798a
Numbersdontlie <luifer92@gmail.com>
2024-04-06 08:45:00 +0200

    modification of the execution pipeline, new error file addedfor execution

--- src/exec/executor.c
+++ src/exec/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lperez-h <lperez-h@student.42.fr>          +#+  +:+       +#+        */
+/*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 15:36:49 by aarponen          #+#    #+#             */
-/*   Updated: 2024/04/04 19:15:13 by lperez-h         ###   ########.fr       */
+/*   Updated: 2024/04/05 11:41:38 by luifer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	**ft_convert_env_list_to_array(t_env *env, t_cmd *cmds)
 		return (NULL);
 	i = 0;
 	tmp = env;
-	while (env)
+	while(env)
 	{
 		result[i] = ft_strjoin(env->var, env->value, cmds->data);
 		i++;
@@ -37,7 +37,7 @@ char	**ft_convert_env_list_to_array(t_env *env, t_cmd *cmds)
 	}
 	result[size] = NULL;
 	env = tmp;
-	return (result);
+	return(result);
 }
 
 //Function to execute a single command
@@ -66,7 +66,6 @@ void	ft_execute_single_command(t_cmd *cmds)
 			cmds->data->exit_status = 0;
 		}
 		free(env);
-		free(path);
 	}
 }
 
@@ -78,40 +77,41 @@ void	ft_exec_cmd(t_cmd *cmds)
 {
 	char	*path;
 	char	**env;
+	int		status;
 
 	ft_find_cmd_path(cmds, cmds->data);
 	path = ft_strdup(cmds->path, cmds->data);
 	env = ft_convert_env_list_to_array(cmds->data->env, cmds);
 	execve(path, cmds->cmd_arg, env);
-	ft_error_cmd(cmds->data);
-	return ;
 }
 
 //Function to execute commands in a pipeline
-//it receives a command and creates a child process 
-//for it, duplicate the file descriptor and 
-//close the fd not used. It executes the command in the child
-//process and the parent process wait for the output and duplicate 
-//and close the unused fd in the parent process.
+//it iterates through the list of commands and
+//creates a child process for each command and
+//executes a command inside the child process
 void	ft_pipe(t_cmd *cmds)
 {
 	pid_t	pid;
+	t_cmd	*tmp;
+	int		size;
+	int		i;
+
+	size = ft_list_size(cmds);
+	i = 0;
+	tmp = cmds;
 
 	pid = fork();
 	if (pid == -1)
 		ft_error_forking(cmds->data);
 	if (pid == 0)
-	{
-		if ((dup2(cmds->cmd_fd[WRITE_END], STDOUT_FILENO) == -1) || close(cmds->cmd_fd[WRITE_END]) == -1 || close(cmds->cmd_fd[READ_END]) == -1)
-			ft_error_fd(cmds->data);
-		ft_exec_cmd(cmds);
-	}
+		ft_execute_child(pid, cmds);
 	waitpid(pid, NULL, 0);
-	if (dup2(cmds->cmd_fd[READ_END], STDERR_FILENO) == -1 || close(cmds->cmd_fd[READ_END]) == -1 || close(cmds->cmd_fd[WRITE_END]) == -1)
-		ft_error_fd(cmds->data);
+	if (dup2(cmds->cmd_fd[READ_END], STDIN_FILENO == -1))
+		ft_error_dup(cmds->data);
+	if (close(cmds->cmd_fd[READ_END]) || close(cmds->cmd_fd[WRITE_END]) == -1)
+		ft_error_closing(cmds->data);
 	return ;
 }
-
 
 //Function to execute the commands received
 //as arguments in the shell, the function will check
@@ -119,19 +119,19 @@ void	ft_pipe(t_cmd *cmds)
 void	ft_execute_cmds(t_cmd *cmds)
 {
 	t_cmd	*tmp;
-	int		size;
 	int		i;
-
+	int		size;
 
 	size = ft_list_size(cmds);
 	if (size == 1)
 		ft_execute_single_command(cmds);
 	else
 	{
+		i = 0;
 		tmp = cmds;
-		while (tmp)
+		while (i < size && tmp)
 		{
-			//ft_set_pipes(cmds);
+			ft_set_pipes(cmds);
 			ft_create_child_process(cmds);
 			ft_set_fd_for_pipe(cmds, tmp);
 			ft_close_fd_for_pipe(cmds, tmp);
