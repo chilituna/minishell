ee5e81b342e95ab680c5d92590b367096dda34b2
Alise <arponenalise@gmail.com>
2024-04-14 19:30:50 +0200

    cleaned executor.c

--- src/exec/executor.c
+++ src/exec/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aarponen <aarponen@student.berlin42>       +#+  +:+       +#+        */
+/*   By: aarponen <aarponen@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 15:36:49 by aarponen          #+#    #+#             */
-/*   Updated: 2024/04/14 16:41:33 by aarponen         ###   ########.fr       */
+/*   Updated: 2024/04/14 19:11:02 by aarponen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,21 +34,7 @@ int	ft_execute_single_command(t_cmd *cmds)
 		{
 			if (ft_handle_redirs(cmds) == 1)
 				exit (cmds->data->exit_status);
-			if (cmds->builtin)
-			{
-				cmds->builtin(cmds);
-				exit (cmds->data->exit_status);
-			}
-			else
-			{
-				if (!cmds->cmd_arg[0])
-					exit (0);
-				if (ft_find_cmd_path(cmds, cmds->data) == 1)
-					exit (cmds->data->exit_status);
-				cmds->data->envp = ft_convert_env_list_to_array(cmds->data->env, cmds);
-				execve(cmds->path, cmds->cmd_arg, cmds->data->envp);
-				ft_error_executing(cmds->data);
-			}
+			ft_exec_cmd(cmds);
 		}
 		cmds->pid = pid;
 		return (ft_wait_children(cmds));
@@ -62,9 +48,6 @@ int	ft_execute_single_command(t_cmd *cmds)
 //and executes the command.
 void	ft_exec_cmd(t_cmd *cmds)
 {
-	char	*path;
-	char	**env;
-
 	if (cmds->builtin)
 	{
 		cmds->builtin(cmds);
@@ -72,19 +55,19 @@ void	ft_exec_cmd(t_cmd *cmds)
 	}
 	else
 	{
-		ft_find_cmd_path(cmds, cmds->data);
-		path = ft_strdup(cmds->path, cmds->data);
-		env = ft_convert_env_list_to_array(cmds->data->env, cmds);
-		execve(path, cmds->cmd_arg, env);
-		free(path);
-		ft_free_array(env);
+		if (!cmds->cmd_arg[0])
+			exit (0);
+		if (ft_find_cmd_path(cmds, cmds->data) == 1)
+			exit (cmds->data->exit_status);
+		cmds->data->envp = ft_convert_env_list_to_array(cmds->data->env, cmds);
+		execve(cmds->path, cmds->cmd_arg, cmds->data->envp);
 		ft_error_executing(cmds->data);
 	}
 }
 
 //function to execute a child process
 //it will create child process and execute command
-int	ft_execute_childrens(t_cmd *cmds)
+int	ft_execute_children(t_cmd *cmds)
 {
 	int		size;
 	int		i;
@@ -130,71 +113,8 @@ int	ft_execute_cmds(t_cmd *cmds)
 	else
 	{
 		ft_create_pipes_and_init(cmds);
-		exit_code = ft_execute_childrens(cmds);
+		exit_code = ft_execute_children(cmds);
 	}
 	cmds->data->exit_status = exit_code;
 	return (exit_code);
 }
-
-/*
-//Function to execute a single command
-//it creates a child process and executes
-//the command with the PATH
-void	ft_execute_single_command(t_cmd *cmds)
-{
-	pid_t	pid;
-
-	printf("lets go");
-	ft_check_here_doc(cmds);
-	printf("heredoc cheked");
-	if (cmds->redir->in)
-		ft_redirect_input(cmds);
-	if (cmds->redir->out)
-		ft_redirect_output(cmds);
-	if (cmds->builtin)
-		cmds->builtin(cmds);
-	else
-	{
-		printf("hello");
-		pid = fork();
-		if (pid == 0)
-			ft_exec_cmd(cmds);
-		else
-		{
-			wait(NULL);
-			cmds->data->exit_status = 0;
-		}
-	}
-}
-
-//Function to execute commands in a pipeline
-//it iterates through the list of commands and
-//creates a child process for each command and
-//executes a command inside the child process
-void	ft_pipes(t_cmd *cmds)
-{
-	t_cmd	*tmp;
-	pid_t	pid;
-	int		size;
-	int		i;
-
-	size = ft_list_size(cmds);
-	i = 0;
-	tmp = cmds;
-	while (i < size)
-	{
-		if ((i < size - 1) && (pipe(cmds->data->pipe_fd[i]) == -1))
-			ft_error_piping(cmds->data);
-		pid = fork();
-		if (pid == -1)
-			ft_error_forking(cmds->data);
-		else if (pid == 0)
-		{
-
-		}
-
-	}
-
-}
-
-*/
