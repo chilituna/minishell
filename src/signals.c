3e9a7ad675ac037eb7779522a316f2656d85acbb
Luis Fernando Perez Herrera <lperez-h@c3a1c3.42berlin.de>
2024-04-16 15:42:51 +0200

    changing sigaction for signal to check behavior of signals. Still not working for heredoc and shell inside of a shell

--- src/signals.c
+++ src/signals.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aarponen <aarponen@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: lperez-h <lperez-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 09:50:14 by aarponen          #+#    #+#             */
-/*   Updated: 2024/03/28 16:41:38 by aarponen         ###   ########.fr       */
+/*   Updated: 2024/04/16 15:40:26 by lperez-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 
 //interrupt the command and display a new prompt on a new line
 void	ft_interrupt(int signal)
@@ -47,11 +48,13 @@ void	ft_signals_interactive(void)
 	struct sigaction	sa_quit;
 
 	memset(&sa_int, 0, sizeof(sa_int));
-	sa_int.sa_handler = &ft_new_prompt;
-	sigaction(SIGINT, &sa_int, NULL);
+	signal(SIGINT, ft_new_prompt);
+	//sa_int.sa_handler = &ft_new_prompt;
+	//sigaction(SIGINT, &sa_int, NULL);
 	memset(&sa_quit, 0, sizeof(sa_quit));
-	sa_quit.sa_handler = SIG_IGN;
-	sigaction(SIGQUIT, &sa_quit, NULL);
+	signal(SIGQUIT, SIG_IGN);
+	//sa_quit.sa_handler = SIG_IGN;
+	//sigaction(SIGQUIT, &sa_quit, NULL);
 }
 
 // ctrl-C: Interrupts the current command and
@@ -66,9 +69,45 @@ void	ft_signals_running(void)
 	struct sigaction	sa_quit;
 
 	memset(&sa_int, 0, sizeof(sa_int));
-	sa_int.sa_handler = &ft_interrupt;
-	sigaction(SIGINT, &sa_int, NULL);
+	signal(SIGINT, ft_interrupt);
+	//sa_int.sa_handler = &ft_interrupt;
+	//sigaction(SIGINT, &sa_int, NULL);
 	memset(&sa_quit, 0, sizeof(sa_quit));
-	sa_quit.sa_handler = &ft_interrupt;
-	sigaction(SIGQUIT, &sa_quit, NULL);
+	signal(SIGQUIT, ft_interrupt);
+	//sa_quit.sa_handler = &ft_interrupt;
+	//sigaction(SIGQUIT, &sa_quit, NULL);
 }
+
+/*
+void	ft_reset_prompt(int signum)
+{
+	(void)signum;
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	write(STDOUT_FILENO, "\n", 1);
+	rl_redisplay();
+}
+
+void	ft_new_line(int signum)
+{
+	if (signum == SIGQUIT)
+		ft_putstr_fd(RED"Quit (core dumped)\n"RESET, STDOUT_FILENO);
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+}
+
+void	ft_signals_interactive(void)
+{
+	signal(SIGINT, ft_reset_prompt);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	ft_signals_non_interactive(void)
+{
+	signal(SIGINT, ft_new_line);
+	signal(SIGQUIT, ft_new_line);
+}
+*/
+
+
+
